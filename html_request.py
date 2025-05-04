6e4c121e2f77fd43e42b4a3c338be347fae2efb3
raphagavino <raph4gavino@gmail.com>
2025-05-04 18:25:01 -0300

    first commit, 28minutes per scan

---
+++ html_request.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+import requests
+import time 
+from os import system
+
+start_time = time.time()
+fails = []
+
+with open('domains_list.txt', 'r') as lista:
+    lista = lista.readlines()
+
+for site in lista:
+    site = site.strip() #sem o strip fica dando erro de %0a no dominio por causa do \n 
+    #system(f"ping -c 1 {site}")
+    
+    try:
+        html = requests.get(f'https://{site}', timeout=5)
+        print(site, html)
+    except requests.exceptions.RequestException as e:
+        print(f"Erro no {site}: {e}")
+        fails.append(site)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"\n Script levou {elapsed_time} segundos")
+print(f"Dominios que deram errado sao: {fails}")
+
+
